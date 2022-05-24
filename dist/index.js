2d113b03bd176a9e322faa770009a0cee8e3ebe6
Owen Andrews <owen@owenandre.ws>
2022-05-24 10:55:13 +1000

    Expanded support for all API methods

--- dist/index.js
+++ dist/index.js
@@ -6153,14 +6153,72 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(935);
 const countapi = __nccwpck_require__(537);
 
+const method = core.getInput('method');
 const namespace = core.getInput('namespace');
 const key = core.getInput('key');
 
-countapi.hit(namespace, key).then((result) => {
-  core.setOutput("value", result.value);
-}).catch((error) => {
-  core.setFailed(error.message);
-});
+switch (method) {
+  case 'get':
+    countapi.get(namespace, key).then((result) => {
+      setOutput("result", result);
+    }).catch((error) => {
+      setFailed(error.message);
+    });
+    break;
+  case 'set':
+    countapi.set(namespace, key, core.getInput('value')).then((result) => {
+      setOutput("result", result);
+    }).catch((error) => {
+      setFailed(error.message);
+    });
+    break;
+  case 'update':
+    countapi.update(namespace, key, core.getInput('amount')).then((result) => {
+      setOutput("result", result);
+    }).catch((error) => {
+      setFailed(error.message);
+    });
+    break;
+  case 'hit':
+    countapi.update(namespace, key).then((result) => {
+      setOutput("result", result);
+    }).catch((error) => {
+      setFailed(error.message);
+    });
+    break;
+  case 'create':
+    countapi.create({ namespace, key, value: core.getInput('input'), enable_reset: core.getBooleanInput('enable_reset'), update_lowerbound: core.getInput('update_lowerbound'), update_upperbound: core.getInput('update_upperbound') }).then((result) => {
+      setOutput("result", result);
+    }).catch((error) => {
+      setFailed(error.message);
+    });
+    break;
+  case 'info':
+    countapi.update(namespace, key).then((result) => {
+      setOutput("result", result);
+    }).catch((error) => {
+      setFailed(error.message);
+    });
+    break;
+  case 'stats':
+    countapi.update().then((result) => {
+      setOutput("result", result);
+    }).catch((error) => {
+      setFailed(error.message);
+    });
+    break;
+  default:
+    setFailed(`Unknown method \`${method}\``);
+}
+
+function setFailed(message) {
+  core.setFailed(message);
+}
+
+function setOutput(name, value) {
+  core.info(JSON.stringify(value));
+  core.setOutput(name, value);
+}
 })();
 
 module.exports = __webpack_exports__;
